2b10796799c023d9d22ca028ac09895f9f901da9
Yirklum <yirklum@aol.com>
2018-12-26 12:47:45 -0500

    JAWS

--- server.js
+++ server.js
@@ -25,13 +25,18 @@ require("./routes/api-routes.js")(app);
 // Set up mysql
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "contacts_DB"
-});
+  }); 
+};
 
 connection.connect(function(err) {
   if (err) {
